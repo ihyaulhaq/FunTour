76dfada08ea76a40448ae26ca752196b131ac9ec
ihyaulhaq <22523278@students.uii.ac.id>
2023-11-24 22:27:57 +0700

    first

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'C:/xampp/htdocs/FunTour/connect_db.php';
+require_once '/FunTour/connect_db.php';
 session_start();
 ?>
 <!DOCTYPE html>
@@ -10,7 +10,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <title>Document</title>
+  <title>Funtour</title>
   <link rel="stylesheet" href="style-home.css">
 </head>
 
